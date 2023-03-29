b132a3fbfbce3f68cd9a861cc9455a1154c13e96
Sasank Madineni <sasank1020@gmail.com>
2023-03-28 19:18:52 -0700

    initial godot changes

---
+++ Scripts/Player.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Player : Node
+{
+
+	private int max_health = 200;
+	private int curr_health;
+	private int max_stamina = 200;
+	private int curr_stamina;
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
